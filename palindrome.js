fc60c8f99504f5ccd10729e29d0adc5f0e25211f
athar05 <atharshakeel96@gmail.com>
2021-09-14 19:51:17 +0530

    updates

--- palindrome.js
+++ palindrome.js
@@ -12,3 +12,27 @@ function isPalindrome(str) {
   }
   return false;
 }
+
+var date = {
+  day: 05,
+  month: 9,
+  year: 2020,
+};
+
+function convertNumToStr(date) {
+  var dateStr = { day: "", month: "", year: "" };
+  if (date.day < 10) {
+    dateStr.day = "0" + date.day;
+  } else {
+    dateStr.day = date.day.toString();
+  }
+  if (date.month < 10) {
+    dateStr.month = "0" + date.month;
+  } else {
+    dateStr.month = date.month.toString();
+  }
+  dateStr.year = date.year.toString();
+
+  return dateStr;
+}
+  
